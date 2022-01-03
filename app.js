473db0808f1500e729bb5317d8937beac66562cd
Saheb Dhara <sahebdhara961@gmail.com>
2022-01-03 22:10:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const inputtodo = document.querySelectorAll(".todu-input")
+const buttontodo = document.querySelectorAll(".tudu-btn")
+const listodo = document.querySelectorAll(".todo-list")
+const ibtn = document.querySelector(".fas fa-audio-description")
+ibtn.addEventListener("click", function(){
+    document.getElementById("demo").innerHTML = "Hello World";
+  });
+  
+  
+// ibtn.addEventListener("click", function (){
+//     //console.log("dgf")
+//     // event.preventDifault()
+//     console.log("hi")
+    // create div
+//     const tododiv = document.createElement("div")
+//     tododiv.classList.add("todo")
+// // create li
+//     const newtodo = document.createElement('li')
+//     newtodo.innerText = 'hay'
+//     newtodo.classList.add("todo-item")
+//     tododiv.appendChild(newtodo)
+   //
+    // const completedBtn = document.createElement('button')
+    // completedBtn.innerText= '<i class ='fas fa-check'></i>'
+    // completedBtn.classList.add("complete-btn")
+    // tododiv.appendChild(completedBtn)
+    
+    // const   trashedBtn = document.createElement('button')
+    // trashedBtn.innerText= "asd"
+    // trashedBtn.classList.add("complete-btn")
+    // tododiv.appendChild(trashedBtn)
+   
+    // listodo.appendChild(tododiv)
+// })
